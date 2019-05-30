d5f325abdb055710030992649ff7aa6649cb7067
Emily Creamer <emily.creamer@icloud.com>
2019-05-30 14:50:12 -0700

    refactored with promiseMiddleware

--- src/actions/charactersActions.js
+++ src/actions/charactersActions.js
@@ -3,22 +3,30 @@ import { getCharacters } from '../services/avatarApi';
 export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 export const FETCH_CHARACTERS_ERROR = 'FETCH_CHARACTERS_ERROR';
-export const fetchCharacters = () => dispatch => {
-  dispatch({
-    type: FETCH_CHARACTERS_LOADING
-  });
 
-  return getCharacters()
-    .then(character => {
-      dispatch({
-        type: FETCH_CHARACTERS,
-        payload: character
-      });
-    })
-    .catch(err => {
-      dispatch({
-        type: FETCH_CHARACTERS_ERROR,
-        payload: err
-      });
-    });
-};
+// export const fetchCharacters = () => dispatch => {
+//   dispatch({
+//     type: FETCH_CHARACTERS_LOADING
+//   });
+
+//   return getCharacters()
+//     .then(character => {
+//       dispatch({
+//         type: FETCH_CHARACTERS,
+//         payload: character
+//       });
+//     })
+//     .catch(err => {
+//       dispatch({
+//         type: FETCH_CHARACTERS_ERROR,
+//         payload: err
+//       });
+//     });
+// };
+
+export const fetchCharacters = () => ({
+  type: FETCH_CHARACTERS,
+  pendingType: FETCH_CHARACTERS_LOADING,
+  rejectedType: FETCH_CHARACTERS_ERROR,
+  payload: getCharacters()
+});
